07e8ce08033a81a2ef101f5cedb85d3b068a4ad5
Oseas Moreto <oseas_moreto@hotmail.com>
2021-10-22 09:54:28 -0300

    Adiciona arquivos de tema do projeto

--- App.tsx
+++ App.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import { Text } from 'react-native';
-import { Login } from './src/screens/Login';
+
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular
+} from '@expo-google-fonts/roboto';
+
+import { Home } from './src/screens/Home';
+
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_400Regular
+  });
+
+  if(!fontsLoaded) return <AppLoading />;
+  
   return (
-    <Login />
+    <>
+      <StatusBar 
+        style="light" 
+        translucent 
+        backgroundColor="transparent" 
+      />
+      <Home />
+    </>
   );
 }
