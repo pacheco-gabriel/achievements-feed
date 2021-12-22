ab7b7bc4911fab0a2e18122f3d45d737a7617c7e
oseasmoreto <oseas_moreto@hotmail.com>
2021-12-22 20:49:49 -0300

    Adiciona componente padrao de listagem de games

--- src/screens/Games/index.tsx
+++ src/screens/Games/index.tsx
@@ -3,24 +3,14 @@ import React from 'react';
 import {
   View, Text, ScrollView
 } from 'react-native';
-import { Game } from '../../components/GameComponents/Game';
+import { GameList } from '../../components/GameComponents/GameList';
 
 import { styles } from './styles';
 
 export function Games(){
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-        <Game title="Dragon Ball Z: Kakarot"/>
-        <Game title="Dragon Ball Z: Kakarot"/>
-        <Game title="Dragon Ball Z: Kakarot"/>
-        <Game title="Dragon Ball Z: Kakarot"/>
-        <Game title="Dragon Ball Z: Kakarot"/>
-        <Game title="Dragon Ball Z: Kakarot"/>
-        <Game title="Dragon Ball Z: Kakarot"/>
-        <Game title="Dragon Ball Z: Kakarot" />
-        <Game title="Dragon Ball Z: Kakarot"/>
-      </ScrollView>
+      <GameList />
     </View>
   );
 }
--- src/screens/Games/styles.ts
+++ src/screens/Games/styles.ts
@@ -5,14 +5,10 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'row',
-    paddingHorizontal: 30,
     backgroundColor: COLORS.BLACK_SECONDARY,
     paddingTop: 0,
   },
   title: {
     color: COLORS.WHITE
-  },
-  scrollView: {
-    marginTop: 40
   }
 });
