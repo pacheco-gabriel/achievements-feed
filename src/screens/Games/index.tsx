0a73d45e5a03ef0f720de2b7a37e0e80bb324a16
oseasmoreto <oseas_moreto@hotmail.com>
2021-12-22 21:13:34 -0300

    Adiciona input de busca

--- src/screens/Games/index.tsx
+++ src/screens/Games/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
-import {
-  View, Text, ScrollView
-} from 'react-native';
-import { GameList } from '../../components/GameComponents/GameList';
+import { View, Text }      from 'react-native';
+import { GameList }  from '../../components/GameComponents/GameList';
+import { FormInput } from '../../components/ProfileComponents/FormInput';
 
-import { styles } from './styles';
+import { styles }   from './styles';
 
 export function Games(){
   return (
     <View style={styles.container}>
+      <View style={styles.formInput}>
+      <Text style={styles.title}>Buscar por Jogos</Text>
+        <FormInput title="" placeholder="Buscar..." />
+      </View>
       <GameList />
     </View>
   );
--- src/screens/Games/styles.ts
+++ src/screens/Games/styles.ts
@@ -3,12 +3,18 @@ import { COLORS } from '../../theme';
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'row',
+    flex: 2,
+    flexDirection: 'column',
     backgroundColor: COLORS.BLACK_SECONDARY,
     paddingTop: 0,
   },
+  formInput: {
+    marginTop: 60,
+    paddingHorizontal: 30,
+  },
   title: {
+    marginBottom: 10,
+    fontSize: 18,
     color: COLORS.WHITE
   }
 });
